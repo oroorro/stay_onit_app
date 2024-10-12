340d3f12e6d5856f00207375b20d41da774c8ff3
Oro Kang <orogang00@gmail.com>
2024-10-12 18:28:01 -0400

    _MiddleView, _TopNav became StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -123,15 +123,7 @@ class _MiddleView extends StatelessWidget {
 }
 
 
-// class _TopNav extends StatefulWidget {
-//   @override
-//   State<_TopNav> createState() => _TopNavState();
-// }
 
-// class _MiddleView extends StatefulWidget {
-//   @override
-//   State<_MiddleView> createState() => _MiddleViewState();
-// }
 
 
 class _CanvasPainter extends CustomPainter {
@@ -146,59 +138,3 @@ class _CanvasPainter extends CustomPainter {
   }
 }
 
-
-
-
-
-
-
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-   
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-
-//         child: Column(
- 
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
